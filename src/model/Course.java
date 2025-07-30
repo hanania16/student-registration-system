0fc2e7d917cbb841d4f3a9ca1831a18641ffa46f
yeabsira377 <yeabsiramitiku13@gmail.com>
2025-07-30 13:20:51 +0300

    work on course class

---
+++ src/model/Course.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Course {
+    private String courseId;
+    private String courseName;
+
+    public Course(String courseId, String courseName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
