322f2a51b7ac0575b91d21b6b1e268e8475ec4e1
yeabsira377 <yeabsiramitiku13@gmail.com>
2025-08-05 08:13:23 +0300

    update

--- src/util/ConfigReader.java
+++ src/util/ConfigReader.java
@@ -20,9 +20,6 @@ public class ConfigReader {
             System.err.println("❌ Failed to load database config file.");
             e.printStackTrace();
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 
 public class ConfigReader {
     private static Properties props = new Properties();
